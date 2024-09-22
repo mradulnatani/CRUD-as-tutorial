1277e1d6708ea1728d5bd7d1ea521cca464e37f6
mradulnatani <mradulnatni0@gmail.com>
2024-09-23 01:22:55 +0530

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,95 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import Note from './components/Note'
-import noteService from './notes'
-const App=()=>{
-  const [notes, setNotes] = useState([])
-    const[newNote,setNewnote] = useState()
-  const[showAll,setShowAll] = useState(true)
-
+import { useState, useEffect } from "react";
+import Note from "./components/Note";
+import noteService from "./notes";
+import Notification from "./notification";
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("some error happened...");
   useEffect(() => {
-    noteService
-      .getAll()
-      .then(initialNotes => {setNotes(initialNotes)})
-  }, [])
-        console.log('render', notes.length, 'notes')
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+  }, []);
+
+  console.log("render", notes.length, "notes");
 
   const addNote = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
       id: String(notes.length + 1),
-    }
-   
-    noteService.create(noteObject).then(returnedNote => {setNotes(notes.concat(returnedNote))     
-       setNewNote('')})
-  }
+    };
 
-  const handleNoteChange=(event)=>{
-    console.log(event.target.value)
-    setNewnote(event.target.value)
-  }
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
+  };
 
-  const toggleImportanceOf = id => {
-    const url = `http://localhost:3001/notes/${id}`
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
-  
-    noteService.update(id, changedNote).then(returnedNote => {
-      setNotes(notes.map(note => note.id !== id ? note : returnedNote))})
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
 
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
 
-  return(
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+      })
+      .catch((error) => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+      });
+  };
+
+  return (
     <>
-    <h1>Notes</h1>
-    <div><button onClick={() => setShowAll(!showAll)}>show 
-      {showAll ? 'important' : 'all' } </button></div>
-    <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} 
-          toggleImportance={() => toggleImportanceOf(note.id)}/>
-        )}
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notes.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
       </ul>
       <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange}/>
-        <button type='submit'>Submit</button>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">Submit</button>
       </form>
+      <Footer/>
     </>
-  )
+  );
+};
 
-}
-}
-export default App 
+export default App;
+
+const Footer = () => {
+  const footerStyle = { color: "green", fontStyle: "italic", fontSize: 16 };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Note app, Department of Computer Science, International Institute of Pre-Human Studies 2024
+      </em>
+    </div>
+  );
+};
--- src/notes.js
+++ src/notes.js
@@ -3,7 +3,12 @@ const baseUrl = 'http://localhost:3001/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+  const nonExisting ={
+    id:1000,
+    content:"This note is been deleted",
+    important:true
+  }
+  return request.then(response => response.data.concat(nonExisting))
 }
 
 const create = newObject => {
